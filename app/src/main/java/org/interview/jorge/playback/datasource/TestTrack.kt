3b6c218917a6ffdc7bae8682b63572fcc82dc807
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2021-09-16 19:39:11 +0200

    Try to play all tracks provided

--- app/src/main/java/org/interview/jorge/playback/datasource/TestTrack.kt
+++ app/src/main/java/org/interview/jorge/playback/datasource/TestTrack.kt
@@ -1,7 +1,12 @@
 package org.interview.jorge.playback.datasource
 
 internal enum class TestTrack(id: String) {
-  TRACK_0("5463901a");
+  TRACK_0("5463901a"),
+  TRACK_1("625ea122"),
+  TRACK_2("85b03a0f"),
+  TRACK_3("aa74e9a1"),
+  TRACK_4("c12afbb7"),
+  TRACK_5("eda70b22");
 
   val metadataDocumentUrl = "https://quc-test-source.s3.amazonaws.com/client-test/$id/play"
 }
